80992fcf22f5f8e3c6d141978e352c1e97e654ad
Odu-Enkay <[erozonankiru@gmail.com]>
2025-04-03 02:01:48 -0600

    Implements the AsserArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,16 @@
+const eqArrays = function (array1, array2) {
+  //check equality in length
+  if (array1.length !== array2.length){
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++){
+    if (array1[i] !== array2[i]){
+      return false;
+    }
+  } 
+  return true;
+  
+};
 //function to assert is two items are equal
 const assertArraysEqual = function (actual, expected) {
   if (actual === expected){
@@ -7,3 +20,5 @@ const assertArraysEqual = function (actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+
