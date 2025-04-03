0ebd33fd7e13b79248e3d956b7025f577ddd470a
Odu-Enkay <[erozonankiru@gmail.com]>
2025-04-03 01:36:22 -0600

    Implemented the function eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+
+const eqArrays = function (array1, array2) {
+  //check equality in length
+  if (array1.length !== array2.length){
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++){
+    if (array1[i] !== array2[i]){
+      return false;
+    }
+  } 
+  return true;
+  
+};
+
+//function to assert is two items are equal
+const assertEqual = function (actual, expected) {
+  if (actual === expected){
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  else{
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
