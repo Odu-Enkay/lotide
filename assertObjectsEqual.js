0d6a46d39951afb980cb7d80a3b796b4a3620d02
Odu-Enkay <[erozonankiru@gmail.com]>
2025-04-04 05:27:57 -0600

    Implements AssrtObjectsEqual function

---
+++ assertObjectsEqual.js
@@ -0,0 +1,38 @@
+// Implement eQObjects function
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) return false;
+
+  for (let key of keys1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// FUNCTION IMPLEMENTATION
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require("util").inspect; 
+  // Implement me!
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+//Test cases
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 2, a: 1 };
+const obj3 = { a: 1, b: 3 };
+
+assertObjectsEqual(obj1, obj2); // Pass
+assertObjectsEqual(obj1, obj3); // Fail
