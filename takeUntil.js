2947c26f76267890b11c102936f7aa8634cac2a7
Odu-Enkay <[erozonankiru@gmail.com]>
2025-04-06 16:23:26 -0600

    implements takeUnitl function

--- takeUntil.js
+++ takeUntil.js
@@ -43,6 +43,6 @@ const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Re
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]); // Make sure the expected array matches what should be returned by takeUntil
 
-assertArraysEqual(takeUntil(data1, x => x < 0));
-assertArraysEqual(takeUntil(data2, x => x === ','));
+assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]); 
