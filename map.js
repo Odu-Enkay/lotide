652f586d8fe73a189ed209fb8b0d8f01d49b4bd2
Odu-Enkay <[erozonankiru@gmail.com]>
2025-04-06 15:31:19 -0600

    Implements map funtion

---
+++ map.js
@@ -0,0 +1,34 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)){
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  else{
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (array1, array2) {
+  //check equality in length
+  if (array1.length !== array2.length){
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++){
+    if (array1[i] !== array2[i]){
+      return false;
+    }
+  } 
+  return true;
+  
+};
+
+map(words);
