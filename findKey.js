b8349e718ad3071cfb8d707fe9ff6522dcf1fad1
Odu-Enkay <[erozonankiru@gmail.com]>
2025-04-06 16:45:31 -0600

    Implements findKey function

---
+++ findKey.js
@@ -0,0 +1,27 @@
+//function to assert is two items are equal
+const assertEqual = function (actual, expected) {
+  if (actual === expected){
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  else{
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  return Object.keys(object).find(key => callback(object[key], key, object));
+}
+
+//Test cases
+console.log(assertEqual(findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+), "noma")); // => "noma"
+
