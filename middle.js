4c27d2708086514e656decdd12f9601945bbce44
Odu-Enkay <[erozonankiru@gmail.com]>
2025-04-08 03:24:41 -0600

    remove test cases and exports function

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length){
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-  
-};
-
-//function to assert is two items are equal
-const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)){
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-  else{
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const middle = function(array) {
   let n = array.length;
@@ -34,10 +12,5 @@ const middle = function(array) {
     return [array[mid - 1], array[mid]];
   }
 };
-//test cases
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+
+module.exports = middle;
