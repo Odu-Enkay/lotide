ab7f00776566230533e0d2fb9a29ab09160a7801
Odu-Enkay <[erozonankiru@gmail.com]>
2025-04-08 02:44:50 -0600

    Improved test codes

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,11 +2,11 @@ const assertEqual = require("../assertEqual");
 const eqArrays = require("../eqArrays");
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 
-eqArrays([1, 2, 3], []); // => false
-eqArrays([], []); // true
+assertEqual(eqArrays([1, 2, 3], []), false); // => false
+assertEqual(eqArrays([], []), true); // true
