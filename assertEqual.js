50df2b76d517b964fea3538c07342c18955fbfa2
Odu-Enkay <[erozonankiru@gmail.com]>
2025-04-08 01:42:13 -0600

    Removed manual test code to add module.export

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,4 @@
+//function to assert is two items are equal
 const assertEqual = function (actual, expected) {
   if (actual === expected){
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -8,10 +9,5 @@ const assertEqual = function (actual, expected) {
 };
 
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("nkiru", "nkiru");
-assertEqual("Nkiru", "nkiru");
-assertEqual(23, 23);
-assertEqual(23, "23");
+//Export
+module.exports = assertEqual;
