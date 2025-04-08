dfbe93c4f3925c9281a783f385dd24981404a01a
Odu-Enkay <[erozonankiru@gmail.com]>
2025-04-08 04:13:20 -0600

    removed unncessary comments

--- head.js
+++ head.js
@@ -1,7 +1,3 @@
-//function to assert is two items are equal
-const assertEqual = require("./assertEqual");
-
-
 //create a function head that returns its first element
 const head = function(array){
   return array[0];
