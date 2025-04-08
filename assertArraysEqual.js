f2debbb5ef46c889b74e27f34c01fa61b0ae2859
Odu-Enkay <[erozonankiru@gmail.com]>
2025-04-08 03:01:34 -0600

    remove test cases to add module.export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function (array1, array2) {
-  //check equality in length
-  if (array1.length !== array2.length){
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++){
-    if (array1[i] !== array2[i]){
-      return false;
-    }
-  } 
-  return true;
-  
-};
+const eqArrays = require("../eqArrays");
 //function to assert is two items are equal
 const assertArraysEqual = function (actual, expected) {
   if (eqArrays(actual, expected)){
@@ -21,7 +9,5 @@ const assertArraysEqual = function (actual, expected) {
   }
 };
 
-//Test cases
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // Assertion Passed
-assertArraysEqual([5, 4, 6], ["5", "4", "6"]); // Assertion Failed
+module.exports = assertArraysEqual;
 
