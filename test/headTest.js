a9104952f7e4e1bdc1304aa688e993e3fea608bd
Odu-Enkay <[erozonankiru@gmail.com]>
2025-04-08 04:18:55 -0600

    refactor test cases with Mocha abd chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,4 @@
-const assertEqual = require("../assertEqual");
 const head = require("../head");
-
-
 const assert = require('chai').assert;
 
 describe("#head", () => {
@@ -17,8 +14,8 @@ describe("#head", () => {
     assert.strictEqual(head([5, 6, 7]), 5);
   });
 
-  it("returns 5 for [5, 6, 7]", () => {
-    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "labs");
+  it("returns Hello for [Hello, Lighthouse, Labs]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
   });
 
 });
